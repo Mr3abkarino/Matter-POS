e8eb2e54ad20dfbe13219f24327331bc6c94bc53
Mr3abkarino <mr3abkarinosoft@gmail.com>
2026-08-06 05:19:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // إضافة الهاش لأسماء الملفات لتفادي الكاش
+        entryFileNames: `assets/[name].${pkg.version}.[hash].js`,
+        chunkFileNames: `assets/[name].${pkg.version}.[hash].js`,
+        assetFileNames: `assets/[name].${pkg.version}.[hash].[ext]`
+      }
+    }
+  }
 })
