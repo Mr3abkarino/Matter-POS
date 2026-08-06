766adcca9191c79e3158d920ef1b0f17d3263880
Mr3abkarino <mr3abkarinosoft@gmail.com>
2026-08-06 05:18:31 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,17 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Tajawal', 'sans-serif'],
+        sans: ['Cairo', 'sans-serif'],
       },
+      colors: {
+        brand: {
+          50: '#eef2ff',
+          100: '#e0e7ff',
+          500: '#6366f1',
+          600: '#4f46e5',
+          700: '#4338ca',
+        }
+      }
     },
   },
   plugins: [],
